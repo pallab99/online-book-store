a2af83de3a1c443b49430e1641e13232d4554aef
pallab-BJIT <pallab.majumdar@bjitacademy.com>
2023-10-10 11:13:57 +0600

    fixed user by id logic

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -170,9 +170,9 @@ class UserController {
                 return sendValidationError(res, validation);
             }
 
-            const { name, rank, isVerified } = req.body;
+            const { name, rank, isVerified, address } = req.body;
             const { userId } = req.params;
-            const allowedProperties = ['name', 'rank', 'isVerified'];
+            const allowedProperties = ['name', 'rank', 'isVerified', 'address'];
 
             for (const key in req.body) {
                 if (!allowedProperties.includes(key)) {
@@ -184,7 +184,7 @@ class UserController {
                 }
             }
 
-            const result = await authModel.findById(userId);
+            const result = await userModel.findById(userId);
             if (!result) {
                 return sendResponse(
                     res,
@@ -192,30 +192,31 @@ class UserController {
                     'No user associated with this id'
                 );
             }
-            const user = await userModel.findById(result.user);
+            console.log('hhhh', result);
+            // const user = await authModel.findOne({ email: result.email });
+            // console.log('gggg', user);
             if (name) {
-                user.name = name;
+                result.name = name;
             }
-            if (rank) {
-                result.rank = rank;
+            // if (rank) {
+            //     user.rank = rank;
+            // }
+            // if (isVerified === true || isVerified === false) {
+            //     user.isVerified = isVerified;
+            //     console.log(result);
+            // }
+            if (address) {
+                result.address = address;
             }
-            if (isVerified === true || isVerified === false) {
-                result.isVerified = isVerified;
-                console.log(result);
-            }
-            const updatedUser = await user.save();
-            const updatedResult = await result.save();
+            const updatedUser = await result.save();
+            // const updatedResult = await user.save();
 
-            if (updatedResult || updatedUser) {
-                const newResult = await authModel
-                    .findById(userId)
-                    .select('-password')
-                    .populate('user', '-email -phoneNumber');
+            if (updatedUser) {
                 return sendResponse(
                     res,
                     HTTP_STATUS.OK,
                     'Updated user successfully',
-                    newResult
+                    []
                 );
             }
             return sendResponse(
